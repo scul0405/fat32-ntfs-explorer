d9b368ef403546eb5c2ccfe800bd7cc12dd70b3c
Nhefm <minhdao2332@gmail.com>
2023-10-31 21:01:58 +0700

    sửa vẽ cây (không dùng hàm thư viện), thêm lại storage vào Entry, comment code, thêm menu

--- source/core/FAT32.py
+++ source/core/FAT32.py
@@ -2,9 +2,10 @@ from enum import Flag, auto
 from base64 import decode
 from itertools import chain
 import binascii
-from treelib import Node, Tree
+import os
 from utils import open_windows_partition
 
+# file attribute got from offset 0B
 class Attribute(Flag):
     Read_Only = auto()
     Hidden = auto()
@@ -13,13 +14,16 @@ class Attribute(Flag):
     Directory = auto()
     Archive = auto()
 
+# Store FATs to get cluster chain from given first cluster of file
 class FAT:
+  # Store FATs
   def __init__(self, data) -> None:
     self.FAT_TABLE = []
 
     for i in range(0, len(data), 4):
       self.FAT_TABLE.append(int.from_bytes(data[i:i + 4], 'little'))
-  
+
+  # Get cluster chain from given first cluster of file
   def get_cluster_chain(self, index: int) -> 'list[int]':
     cluster_chain = []
     # print('///')
@@ -31,8 +35,10 @@ class FAT:
         break
       
     return cluster_chain
-  
+
+# Entry aka File information
 class Entry:
+    # Store File information
     def __init__(self, data) -> None:
         self.raw_data = data
         self.flag = data[0xB:0xC]
@@ -46,6 +52,7 @@ class Entry:
         self.total_name = ""
         self.file_content = ""
         self.display = ""
+        self.storage = []
         self.parse_entry(data)
 
     # Parse the entry data
@@ -57,7 +64,7 @@ class Entry:
         else:
             self.parse_subentry(data)
 
-    # Parse the main entry
+    # Parse main entry
     def parse_main_entry(self, data):
         self.name = data[:0x8]
         self.ext = data[0x8:0xB]
@@ -76,6 +83,7 @@ class Entry:
         self.start_cluster = int.from_bytes(data[0x1A:0x1C], 'little')
         self.size = int.from_bytes(data[0x1C:0x20], 'little')
 
+    # Parse sub entry
     def parse_subentry(self, data):
         self.name = b""
         for i in chain(range(0x1, 0xB), range(0xE, 0x1A), range(0x1C, 0x20)):
@@ -85,20 +93,17 @@ class Entry:
                 break
         self.name = self.name.decode('utf-16le').strip('\x00')
 
+    # Check if it is main entry
     def is_main_entry(self) -> bool:
         return not (self.is_empty or self.is_subentry or self.is_deleted or self.is_label or Attribute.System in self.attr)
 
+    # Check if it is directory
     def is_directory(self) -> bool:
         return Attribute.Directory in self.attr
 
+    # The second line of file when display, show attribute and sectors
     def set_display_in_tree(self, chain, SC):
-        self.display = self.total_name + ":\t"
-
-        if(self.size != 0):
-            self.display = self.display + "size: " + str(self.size) + "\t"
-
         self.display = self.display + self.display_attr()
-
 
         if (chain):
             self.display = self.display + "\t Sectors: " + str(chain[0] * SC) + " -> " + str((chain[0] + len(chain)) * SC - 1)
@@ -135,7 +140,9 @@ class Entry:
 
         return display
 
+# Store list of Entries, including main and sub entries
 class DET:
+  # Get all raw data and convert it into list Entries
   def __init__(self, data: bytes) -> None:
     self.entries: list[Entry] = []
     total_name = ""
@@ -173,11 +180,12 @@ class DET:
     # Save all main entries of a det
     self.list_main_entries: 'list[Entry]' = [entry for entry in self.entries if entry.is_main_entry()]
 
-
+# Main class, store all other components
 class FAT32:
+    # Read and store reserved data (including bootsector), FATs, RDET
     def __init__(self, drive_name: str) -> None:
         self.bootsector_data_raw = None
-        
+        # Open disk
         try:
             self.drive = open(r'\\.\%s:' % drive_name, 'rb')
             print(f"Reading {drive_name}...")
@@ -208,24 +216,21 @@ class FAT32:
 
             # RDET data
             self.RDET_data_raw = self.drive.read(self.SC * self.BPS)
-            
-            # create tree
-            self.tree = Tree()
-            self.total_node = 0
-            drive_display = Entry(b'')
-            drive_display.display = drive_name + ":"
-            self.tree.create_node(drive_name + ":" , self.total_node, data=drive_display)
-            self.total_node = self.total_node + 1
 
-            # get list file
-            self.list_File = []
-            self.get_all_files(self.RDET_data_raw, 0)
+            # Create root
+            self.disk = Entry(b'')
+            self.disk.total_name = drive_name + ":"
+            self.disk.storage = self.get_all_files(self.RDET_data_raw)
+
+            # found file/folder flag
+            self.found_file = False
 
             print('Read Success')    
         except FileNotFoundError:
             print("Drive Not Found")
             exit(1)
-
+    
+    # Read all disk information in bootsector
     def get_bootsector_discription(self):
         # File System type  offset: 52h size: 8 bytes
         self.file_type = self.bootsector_data_raw[0x52:0x5A].decode('utf-8')
@@ -253,7 +258,8 @@ class FAT32:
 
         # First Data Sector = SB + NF * SF
         self.SDATA = self.SB + self.NF * self.SF
-
+    
+    # Print offset of given raw data (Ex: bootsector, FATs, RDET, ...)
     def print_table_offset(self, data) -> None:
         str_data = binascii.hexlify(data).decode("utf-8")
 
@@ -287,6 +293,7 @@ class FAT32:
                     print(".", end='')
             print()
 
+    # Print all disk information got from bootsector
     def print_bst_info(self):
         print("FAT type: " + self.file_type)
 
@@ -308,25 +315,22 @@ class FAT32:
         print("First Sector of FAT: " + str(self.SB))
 
         print("First Sector of Data: " + str(self.SDATA))
-    
-    def get_all_files(self, data, idx):
-        list_File = DET(data).list_main_entries
 
-        for i in list_File:
-            self.tree.create_node(i.total_name, self.total_node, idx, i)
+    # Get all files/folders from data (RDET, SDET) given (Mutually recursive with get_folder_content)
+    def get_all_files(self, data):
+        list_File = []
 
-            current_file_idx = self.total_node
-            self.total_node = self.total_node + 1
-            self.list_File.append(i)
+        for i in DET(data).list_main_entries:
+            list_File.append(i)
 
             if(i.is_directory()):
-                self.get_folder_content(i, current_file_idx)
+                self.get_folder_content(i)
             else:
                 self.get_file_content(i)
 
-            
-
-
+        return list_File
+    
+    # Get content of file from given list entries
     def get_file_content(self, file: Entry):
         if(file.size == 0):
             file.set_display_in_tree([], self.SC)
@@ -335,7 +339,7 @@ class FAT32:
         chain = self.FAT_data.get_cluster_chain(file.start_cluster)
         file.set_display_in_tree(chain , self.SC)
         size_remaining = file.size
-        
+
         if(file.ext.decode('utf-8') == 'TXT'):
             for i in chain:
 
@@ -351,7 +355,8 @@ class FAT32:
         else:
             file.file_content = "Use other compatible app to run this file!"
     
-    def get_folder_content(self, folder: Entry, idx):
+    # Get content of folder from given list entries (all files/folders contain in it) (Mutually recursive with get_all_files)
+    def get_folder_content(self, folder: Entry):
         # print(folder.total_name)
         chain = self.FAT_data.get_cluster_chain(folder.start_cluster)
         raw_data = b''
@@ -365,26 +370,83 @@ class FAT32:
             read_data = self.drive.read(byte_per_cluster)
             raw_data = raw_data + read_data
         
-        folder.storage = self.get_all_files(raw_data[64:], idx)
-    
+        folder.storage = self.get_all_files(raw_data[64:])
+
     # From cluster index to sector index
     def cluster_to_sector(self, index):
         return self.SB + self.SF * self.NF + (index - 2) * self.SC
 
-    # find entry whose name match the given name
-    def find_file(self, name) -> Entry:
-        print("Reading " + name + "...")
-
-        for i in range(len(self.list_File)):
-
-            if self.list_File[i].total_name == name:
-
-                if self.list_File[i].is_directory():
-                    tree = self.tree.subtree(i + 1)
-                    tree.show(data_property="display")
-                else:
-                    print(self.list_File[i].file_content)
-
-                return
+    # Find file matches the given name
+    def find_file(self, name):
+        self.found_file = False
+        self.find_file_in_folder(name, self.disk.storage)
         
-        print("No file found.")
+        if self.found_file == False:
+            print("No file found.")
+
+    # Find file in folder matches given name (Recursion)
+    def find_file_in_folder(self, name, folder):
+        if self.found_file:
+            return
+
+        for i in folder:
+            if i.total_name == name:
+                self.found_file = True
+                print("Reading " + name + "...")
+                if i.is_directory():
+                    self.print_tree(i)
+                else:
+                    print(i.file_content)
+                return
+            elif i.is_directory():
+                self.find_file_in_folder(name, i.storage)
+
+    # Print all children nodes in the same depth and go to the next depth(Recursion)
+    def print_children_tree(self, list, indent='', elbow='└──', pipe='│  ', tee='├──', blank='   '):
+        for i in list:
+            is_last_child = (i == list[len(list) - 1])
+
+            if i.is_directory():
+                print(indent + (elbow if is_last_child else tee) + i.total_name)
+                next_indent = indent + (blank if is_last_child else pipe)
+                print(next_indent + i.display)
+                self.print_children_tree(i.storage, next_indent, elbow, pipe, tee, blank)
+            else:
+                print(indent + (elbow if is_last_child else tee) + i.total_name + '\tsize: ' + str(i.size if i.size != 0 else 0))
+                print(indent + (blank if is_last_child else pipe) + i.display)
+
+    # Print tree with given root
+    def print_tree(self, folder):
+        print(folder.total_name)
+
+        if folder.display:
+            print(folder.display) 
+
+        self.print_children_tree(folder.storage)
+
+def FAT32_display():
+    drive = input("Select disk: ")
+    fat32 = FAT32(drive)
+
+    while(1):
+        os.system("cls")
+        print("----------FAT 32----------")
+        print("1. Read disk information.")
+        print("2. Print folder tree.")
+        print("3. Read selected file/folder.")
+        print("4. Exit.")
+
+        choice = int(input("Select: "))
+
+        match choice:
+            case 1:
+                fat32.print_bst_info()
+            case 2:
+                fat32.print_tree(fat32.disk)
+            case 3:
+                filename = input("Searching file: ")
+                fat32.find_file(filename)
+            case 4:
+                break
+
+        os.system("pause")
